b8f07cf9c3dc6acfd99d3d1af097da61f3ef3596
coolcyrstal <chayenjr.sk131@gmail.com>
2018-11-14 16:22:48 +0700

    streaming to wowza

--- app/src/main/java/com/example/chayent/samplescreenrecord/MainActivity.java
+++ app/src/main/java/com/example/chayent/samplescreenrecord/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chayent.samplescreenrecord;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.display.DisplayManager;
@@ -16,19 +17,30 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.SparseIntArray;
 import android.view.Surface;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import java.io.IOException;
+import net.majorkernelpanic.streaming.Session;
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.audio.AudioQuality;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.rtsp.RtspClient;
+import net.majorkernelpanic.streaming.video.VideoQuality;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity implements Session.Callback, SurfaceHolder.Callback, RtspClient.Callback {
 
     private static final String TAG = "MainActivity";
     private static final int REQUEST_CODE = 1000;
@@ -44,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
     private static final int REQUEST_PERMISSIONS = 10;
 
+    private Session mSession;
+    private SurfaceView mSurfaceView;
+    private RtspClient mClient;
+    private String mTextUrl = "rtsp://7bfcec.entrypoint.cloud.wowza.com";
+
     static {
         ORIENTATIONS.append(Surface.ROTATION_0, 90);
         ORIENTATIONS.append(Surface.ROTATION_90, 0);
@@ -55,6 +72,12 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSurfaceView = findViewById(R.id.surface);
+
+
+
+
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         mScreenDensity = metrics.densityDpi;
@@ -64,35 +87,34 @@ public class MainActivity extends AppCompatActivity {
         mProjectionManager = (MediaProjectionManager) getSystemService
                 (Context.MEDIA_PROJECTION_SERVICE);
 
-        mToggleButton = (ToggleButton) findViewById(R.id.toggle);
+        mToggleButton = findViewById(R.id.toggle);
         mToggleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE) + ContextCompat
-                        .checkSelfPermission(MainActivity.this,
-                                Manifest.permission.RECORD_AUDIO)
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        + ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        + ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO)
                         != PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale
-                            (MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
-                            ActivityCompat.shouldShowRequestPermissionRationale
-                                    (MainActivity.this, Manifest.permission.RECORD_AUDIO)) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.RECORD_AUDIO)) {
                         mToggleButton.setChecked(false);
-                        Snackbar.make(findViewById(android.R.id.content), "Request permissions",
-                                Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
-                                new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        ActivityCompat.requestPermissions(MainActivity.this,
-                                                new String[]{Manifest.permission
-                                                        .WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO},
-                                                REQUEST_PERMISSIONS);
-                                    }
-                                }).show();
+                        Snackbar.make(findViewById(android.R.id.content), "Request permissions", Snackbar.LENGTH_INDEFINITE)
+                                .setAction("ENABLE",
+                                        new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                ActivityCompat.requestPermissions(MainActivity.this,
+                                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                                Manifest.permission.CAMERA,
+                                                                Manifest.permission.RECORD_AUDIO}, REQUEST_PERMISSIONS);
+                                            }
+                                        }).show();
                     } else {
                         ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission
-                                        .WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO},
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                        Manifest.permission.CAMERA,
+                                        Manifest.permission.RECORD_AUDIO},
                                 REQUEST_PERMISSIONS);
                     }
                 } else {
@@ -100,6 +122,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+//        // Blocks until the all streams are configured
+//        try {
+//            mSession.syncConfigure();
+//        } catch (Exception e) {
+//
+//        }
+//        String sdp = mSession.getSessionDescription();
+//
+//        // Blocks until streaming actually starts.
+//        try {
+//            mSession.syncStart();
+//        } catch (Exception e) {
+//
+//        }
+//        mSession.syncStop();
     }
 
     @Override
@@ -119,6 +157,131 @@ public class MainActivity extends AppCompatActivity {
         mMediaProjection.registerCallback(mMediaProjectionCallback, null);
         mVirtualDisplay = createVirtualDisplay();
         mMediaRecorder.start();
+//        streaming();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS: {
+                if ((grantResults.length > 0) && (grantResults[0] +
+                        grantResults[1]) == PackageManager.PERMISSION_GRANTED) {
+                    onToggleScreenShare(mToggleButton);
+                } else {
+                    mToggleButton.setChecked(false);
+                    Snackbar.make(findViewById(android.R.id.content), "Request permissions",
+                            Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                            new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent = new Intent();
+                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    intent.addCategory(Intent.CATEGORY_DEFAULT);
+                                    intent.setData(Uri.parse("package:" + getPackageName()));
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                                    startActivity(intent);
+//                                    streaming();
+                                }
+                            }).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        destroyMediaProjection();
+    }
+
+    @Override
+    public void onBitrateUpdate(long bitrate) {
+        // Informs you of the bandwidth consumption of the streams
+//        Log.d(TAG, "Bitrate: " + bitrate);
+    }
+
+    @Override
+    public void onSessionError(int reason, int streamType, Exception e) {
+        // Might happen if the streaming at the requested resolution is not supported
+        // or if the preview surface is not ready...
+        // Check the Session class for a list of the possible errors.
+        Log.e(TAG, "An error occured", e);
+        switch (reason) {
+            case Session.ERROR_INVALID_SURFACE:
+                break;
+            case Session.ERROR_STORAGE_NOT_READY:
+                break;
+            case Session.ERROR_CONFIGURATION_NOT_SUPPORTED:
+                VideoQuality quality = mSession.getVideoTrack().getVideoQuality();
+                logError("The following settings are not supported on this phone: " +
+                        quality.toString() + " " +
+                        "(" + e.getMessage() + ")");
+                e.printStackTrace();
+                return;
+            case Session.ERROR_OTHER:
+                break;
+        }
+    }
+
+    @Override
+    public void onPreviewStarted() {
+        Log.d(TAG, "Preview started.");
+    }
+
+    @Override
+    public void onSessionConfigured() {
+        Log.d(TAG, "Preview configured.");
+        // Once the stream is configured, you can get a SDP formated session description
+        // that you can send to the receiver of the stream.
+        // For example, to receive the stream in VLC, store the session description in a .sdp file
+        // and open it with VLC while streming.
+        Log.d(TAG, mSession.getSessionDescription());
+        mSession.start();
+    }
+
+    @Override
+    public void onSessionStarted() {
+        Log.d(TAG, "Streaming session started.");
+    }
+
+    @Override
+    public void onSessionStopped() {
+        Log.d(TAG, "Streaming session stopped.");
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // Starts the preview of the Camera
+        mSession.startPreview();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Stops the streaming session
+        mSession.stop();
+    }
+
+    @Override
+    public void onRtspUpdate(int message, Exception e) {
+        switch (message) {
+            case RtspClient.ERROR_CONNECTION_FAILED:
+            case RtspClient.ERROR_WRONG_CREDENTIALS:
+                // mProgressBar.setVisibility(View.GONE);
+                // enableUI();
+                logError(e.getMessage());
+                e.printStackTrace();
+                break;
+        }
     }
 
     public void onToggleScreenShare(View view) {
@@ -140,6 +303,42 @@ public class MainActivity extends AppCompatActivity {
         }
         mVirtualDisplay = createVirtualDisplay();
         mMediaRecorder.start();
+//        streaming();
+    }
+
+    private void streaming() {
+        if (!mClient.isStreaming()) {
+            String ip, port, path;
+//                Pattern uri = Pattern.compile("rtsp://(.+):(\\d*)/(.+)");
+//                Matcher m = uri.matcher(mTextUrl);
+//                m.find();
+//                ip = m.group(1);
+//                port = m.group(2);
+//                path = m.group(3);
+
+//            try {
+//                mSession.syncConfigure();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+
+
+            mClient.getSession();
+            mClient.setCredentials("chayenEXE", "chayenEXE1234");
+            mClient.setServerAddress("10.5.51.11", 1935);
+            mClient.setStreamPath("/live/myStream");
+            mSession.startPreview();
+            mClient.startStream();
+
+//            try {
+//                mSession.syncStart();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        } else {
+            mSession.stopPreview();
+            mClient.stopStream();
+        }
     }
 
     private VirtualDisplay createVirtualDisplay() {
@@ -155,18 +354,35 @@ public class MainActivity extends AppCompatActivity {
             mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
             mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            mMediaRecorder.setOutputFile(Environment
-                    .getExternalStoragePublicDirectory(Environment
-                            .DIRECTORY_DOWNLOADS) + "/sample_record.mp4");
+            mMediaRecorder.setOutputFile(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/sample_record.mp4");
             mMediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
             mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
             mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            mMediaRecorder.setVideoEncodingBitRate(1920 * 1080);
+            mMediaRecorder.setVideoEncodingBitRate(1080 * 720);
             mMediaRecorder.setVideoFrameRate(30);
             int rotation = getWindowManager().getDefaultDisplay().getRotation();
             int orientation = ORIENTATIONS.get(rotation + 90);
             mMediaRecorder.setOrientationHint(orientation);
             mMediaRecorder.prepare();
+
+            mSurfaceView.getHolder().addCallback(this);
+            mSession = SessionBuilder.getInstance()
+                    .setCallback(this)
+                    .setSurfaceView(mSurfaceView)
+                    .setPreviewOrientation(0)
+                    .setContext(getApplicationContext())
+                    .setAudioEncoder(SessionBuilder.AUDIO_AAC)
+                    .setAudioQuality(new AudioQuality(16000, 32000))
+                    .setVideoEncoder(SessionBuilder.VIDEO_H264)
+                    .setVideoQuality(new VideoQuality(480, 360, 20, 500000))
+//                .setDestination(mTextUrl)
+                    .build();
+
+            mClient = new RtspClient();
+            mClient.setSession(mSession);
+            mClient.setCallback(this);
+
+            streaming();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -196,50 +412,27 @@ public class MainActivity extends AppCompatActivity {
         destroyMediaProjection();
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        destroyMediaProjection();
-    }
-
     private void destroyMediaProjection() {
         if (mMediaProjection != null) {
             mMediaProjection.unregisterCallback(mMediaProjectionCallback);
             mMediaProjection.stop();
             mMediaProjection = null;
         }
+        mClient.release();
+        mSession.release();
+        mSurfaceView.getHolder().removeCallback(this);
         Log.i(TAG, "MediaProjection Stopped");
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_PERMISSIONS: {
-                if ((grantResults.length > 0) && (grantResults[0] +
-                        grantResults[1]) == PackageManager.PERMISSION_GRANTED) {
-                    onToggleScreenShare(mToggleButton);
-                } else {
-                    mToggleButton.setChecked(false);
-                    Snackbar.make(findViewById(android.R.id.content), "Request permissions",
-                            Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
-                            new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    Intent intent = new Intent();
-                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                    intent.addCategory(Intent.CATEGORY_DEFAULT);
-                                    intent.setData(Uri.parse("package:" + getPackageName()));
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                                    startActivity(intent);
-                                }
-                            }).show();
-                }
-                return;
+    private void logError(final String msg) {
+        final String error = (msg == null) ? "Error unknown" : msg;
+        // Displays a popup to report the eror to the user
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(msg).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
             }
-        }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
